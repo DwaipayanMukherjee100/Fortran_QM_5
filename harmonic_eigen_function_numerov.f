3c976ef4ea18c2e8963610839f905c3400442f2e
Dwaipayan Mukherjee <dwaipayanmukherjee100@gmail.com>
2025-12-19 05:44:25 +0530

    Notes updated

---
+++ harmonic_eigen_function_numerov.f
@@ -0,0 +1,9 @@
+        program Harmonic_with_numerov
+c-- this code solved the Time-independent schrodinger equation using numerov method 
+c--- the next version of this code will aim at finding the eigen vales of a given harmonic oscilator 
+c---- in this entire repo , we shall assume \bar{h} =1, m=1 and \omega=1
+        implicit double precision (a-h,o-z)
+        parameter (N=10000)
+        dimension x(N),psi(N)
+        stop 
+        end 
